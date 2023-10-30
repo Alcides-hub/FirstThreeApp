935ced72743dc598394c9769881c1d5f924f9b88
alcides_t <alcides.dml@icloud.com>
2023-10-30 15:02:19 +0900

    Added backgroundColor

--- App.js
+++ App.js
@@ -229,7 +229,7 @@ export default function App() {
         </Canvas>  
         {isDrawerOpen && (
         <TouchableWithoutFeedback onPress={toggleDrawer}>
-          <View style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, alignItems: 'center', justifyContent: 'center'}}>
+          <View style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.7)', justifyContent: 'center'}}>
             <SideDrawer isOpen={isDrawerOpen} items={interactedItems} />
           </View>
         </TouchableWithoutFeedback>
