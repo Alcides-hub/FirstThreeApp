0a072b5363dea7e0035b53836a22d0d08d04af36
alcides_t <alcides.dml@icloud.com>
2023-11-15 15:43:06 +0900

    Added actionable dialogue box, added interactive hotspot, added zoomControls to target

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { useState, useRef, Suspense, useEffect } from 'react';
 import { View, TouchableOpacity, Image, Text, TouchableWithoutFeedback, StyleSheet } from 'react-native';
-import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber/native';
+import { Canvas, useFrame, useLoader, useThree} from '@react-three/fiber/native';
 import * as Three from 'three';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faCrosshairs} from '@fortawesome/free-solid-svg-icons';
@@ -11,9 +11,12 @@ import { TextureLoader } from 'three';
 import SideDrawer from './components/side_drawer';
 import Hotspot from './components/hotspot';
 import ImageModal from './modal/imageModal';
+import Dialoguebox1 from './components/dialogue_box1';
+import ZoomControls from './components/zoomControls';
 
 // import { useAnimatedSensor, SensorType } from 'react-native-reanimated';
 import { Gyroscope } from 'expo-sensors';
+
 
 
 const useGyroscope = (callback) => {
@@ -77,8 +80,12 @@ function CameraControls() {
 
 function ImageSphere() {
   const mesh = useRef();
-  const texture = new Three.TextureLoader().load(require('./assets/art_gallery_1.png'));
- 
+  // useLoader will load the texture and cache it for later use
+  const texture = useLoader(Three.TextureLoader, require('./assets/art_gall_2.png'));
+
+  // If texture is not loaded, don't render the sphere yet
+  if (!texture) return null;
+
 
   return (
     <mesh ref={mesh}>
@@ -90,9 +97,9 @@ function ImageSphere() {
 
 
 
-function EggBox({onInteract}) {
+function EggBox({onInteract, showObject, setShowObject}) {
   const [active, setActive] = useState(false);
-  const [showEggBox, setShowEggBox] = useState(true);
+  
   
   
   const [cavity, diffuse, normals, occlusion, rough] = useLoader(TextureLoader, [
@@ -112,11 +119,13 @@ function EggBox({onInteract}) {
     }
     // If already active (i.e., has been clicked once and is rotating), hide the EggBox
     if (active) {
-      setShowEggBox(false);
+      setShowObject(true);
   }
     setActive(!active);
     
   };
+
+  
 
 
     const obj = useLoader(
@@ -131,7 +140,7 @@ function EggBox({onInteract}) {
     
   }));
 
-  const originalColor = useRef();  // New ref to store original color
+  // const originalColor = useRef();  // New ref to store original color
 
   obj.traverse((child) => {
     if (child instanceof Three.Mesh) {
@@ -140,66 +149,78 @@ function EggBox({onInteract}) {
   });
 
   // Update material color based on 'active' state
-  useEffect(() => {
-    if (active) {
-      // Store the original color when object becomes active
-      originalColor.current = meshMaterial.current.color.clone();
-      meshMaterial.current.color.set('green');
-    } else {
-      // Reset to original color when object becomes inactive
-      if (originalColor.current) {
-        meshMaterial.current.color.copy(originalColor.current);
-      }
-    }
-  }, [active]);
+  // useEffect(() => {
+  //   if (active) {
+  //     // Store the original color when object becomes active
+  //     originalColor.current = meshMaterial.current.color.clone();
+  //     meshMaterial.current.color.set('green');
+  //   } else {
+  //     // Reset to original color when object becomes inactive
+  //     if (originalColor.current) {
+  //       meshMaterial.current.color.copy(originalColor.current);
+  //     }
+  //   }
+  // }, [active]);
+
+  
+
 
   const mesh = useRef();
-const rotationY = useRef(0); // This will keep track of the current Y rotation.
-const targetRotationY = useRef(0);  // New ref to store the target rotation.
+// const rotationY = useRef(0); // This will keep track of the current Y rotation.
+// const targetRotationY = useRef(0);  // New ref to store the target rotation.
 
-useEffect(() => {
-  if (active) {
-      targetRotationY.current = Math.PI;  // 180 degrees in radians.
-  } else {
-      targetRotationY.current = 0;  // Reset rotation.
-  }
-}, [active]);
+// useEffect(() => {
+//   if (active) {
+//       targetRotationY.current = Math.PI;  // 180 degrees in radians.
+//   } else {
+//       targetRotationY.current = 0;  // Reset rotation.
+//   }
+// }, [active]);
 
-const rotationSpeed = 0.03; // Adjust this value for desired rotation speed
+// const rotationSpeed = 0.03; // Adjust this value for desired rotation speed
 
-useFrame((state, delta) => {
-  if (active) {
-    rotationY.current += delta;
-    if (rotationY.current >= 2 * Math.PI) {
-      rotationY.current -= 2 * Math.PI;
-    }
-  } else {
-    rotationY.current -= rotationSpeed;
-    if (rotationY.current <= 0) {
-      rotationY.current = 0;
-    }
-  }
+// useFrame((state, delta) => {
+//   if (active) {
+//     rotationY.current += delta;
+//     if (rotationY.current >= 2 * Math.PI) {
+//       rotationY.current -= 2 * Math.PI;
+//     }
+//   } else {
+//     rotationY.current -= rotationSpeed;
+//     if (rotationY.current <= 0) {
+//       rotationY.current = 0;
+//     }
+//   }
 
-  if (mesh.current) {
-    mesh.current.rotation.y = rotationY.current;
-  }
-});
+//   if (mesh.current) {
+//     mesh.current.rotation.y = rotationY.current;
+//   }
+// });
     
   return (
-    showEggBox &&
+    showObject &&
     <mesh  
     ref={mesh} 
     onClick={handleInteraction}
     position={[0, -5, -30]} 
-    rotation={active ? [0, Math.PI / 2, 0] : [0, 0, 99]}>
+    rotation={[0, 0, 99]}>
       <primitive 
       object={obj} 
-      scale={active ? 0.4 : 0.1} 
+      scale={0.1} 
       />      
     </mesh>
   );
 }  
 
+const SetupCamera = ({ setCameraRef }) => {
+  const { camera } = useThree();
+  
+  useEffect(() => {
+    setCameraRef(camera);
+  }, [camera, setCameraRef]);
+
+  return null; // This component does not render anything
+};
 
 
 export default function App() {
@@ -207,6 +228,38 @@ export default function App() {
   const [isDrawerOpen, setDrawerOpen] = useState(false);
   const [isImageOpen, setIsImageOpen] = useState(false);
   const [CurrentSelectedItem, setCurrentSelectedItem] = useState(null);
+  const [isDialogueVisible, setIsDialogueVisible] = useState(false);
+  const [dialogContent, setDialogContent] = useState(''); // Add this state for the dialogue content
+  const [showObject, setShowObject] = useState(true);
+  const [usedItems, setUsedItems] = useState({}); // New state to track used items
+  // State for the spherical coordinates
+  const [sphericalCoords, setSphericalCoords] = useState(new Three.Spherical());
+// State for the camera's field of view (FOV), which affects zooming
+const [initialFOV, setInitialFOV] = useState(null);
+const [zoomLevel, setZoomLevel] = useState(75); // Default FOV, change as needed for zoom
+const [initialCameraState, setInitialCameraState] = useState({
+  position: new Three.Vector3(),
+  quaternion: new Three.Quaternion(),
+}); 
+const [zoomActive, setZoomActive] = useState(false);
+const cameraRef = useRef(null);
+
+  // Function to update the camera ref
+  const setCameraRef = (camera) => {
+    cameraRef.current = camera;
+    setInitialFOV(cameraRef.current.fov);
+  };
+
+
+  const saveCameraState = () => {
+    if (cameraRef.current) {
+      setInitialCameraState({
+        position: cameraRef.current.position.clone(),
+        quaternion: cameraRef.current.quaternion.clone(),
+        fov: initialFOV, 
+      });
+    }
+  };
 
   const toggleDrawer = () => {
     setDrawerOpen(!isDrawerOpen);
@@ -232,44 +285,113 @@ export default function App() {
     setCurrentSelectedItem(itemName);
   };
 
+  const itemsDetails = {
+    EggBox: "This eggbox is weird, I don't think I have any use of it"
+  };
+  
+  const getItemDetails = (itemName) => {
+    // Look up item details by name or ID
+    return itemsDetails[itemName] || 'No details available.';
+  };
+
   const onLook = (itemName) => {
     // Implement the "Look" functionality based on itemName
     console.log(`Looking at ${itemName}`);
     // Open a modal or show information about itemName
+    setIsDialogueVisible(true);
+    const details = getItemDetails(itemName); 
+    setDialogContent(details); 
   };
   
   const onUseItem = (itemName) => {
+
+    setShowObject(false);
     // Implement the "Use" functionality based on itemName
     console.log(`Using ${itemName}`);
     // Perform an action with itemName
+    setUsedItems(prevUsedItems => ({
+      ...prevUsedItems,
+      [itemName]: true
+    }));
   };
+
+  const handleTouchEnd = (point) => {
+    const spherical = new Three.Spherical().setFromVector3(point);
+    setSphericalCoords(spherical); // Set the spherical coordinates for the zoom
+    setZoomLevel(30); // Set the zoom level you want after touching the hotspot
+    setZoomActive(true);
+  };
+
+  const handleBackButtonClick = () => {
+    
+    if (cameraRef.current) {
+      cameraRef.current.position.copy(initialCameraState.position);
+      cameraRef.current.quaternion.copy(initialCameraState.quaternion);
   
+      // Reset FOV if you're using it to zoom
+      // Assuming initialFOV is a stored value that represents the initial field of view of the camera
+      cameraRef.current.fov = initialFOV;
+      setZoomLevel(initialFOV); 
+      cameraRef.current.updateProjectionMatrix();
+      setZoomActive(false);
+    }
+  
+    // You might also want to reset any other state related to the zoom
+  };
+
+  // useEffect(() => {
+  //   // Calculate spherical coordinates from the target position
+  //   const targetPosition = new Three.Vector3(22, 5, -47);
+  //   const newSphericalCoords = new Three.Spherical().setFromVector3(targetPosition.normalize());
+  //   newSphericalCoords.radius = 60; // Radius of your sphere
+  //   setSphericalCoords(newSphericalCoords);
+
+  //   // Set the zoom level to your desired FOV
+  //   setZoomLevel(30); // Example zoom level, adjust as needed
+  // }, []);
+
 
   return (
     <View style={{ flex: 1 }}>
       <Canvas camera={{ position: [0, 0, 19] }}>
+        <SetupCamera setCameraRef={setCameraRef} />
         <ambientLight />
         <pointLight position={[1, 15, 15]} />
         <CameraControls />
-        <ImageSphere />
         <Suspense fallback={null}>
-          <EggBox onInteract={handleItemInteraction} />
-          <Hotspot onClick={handleItemInteraction} /> 
+        <ImageSphere />
         </Suspense>
+        <Suspense fallback={null}>
+          <EggBox onInteract={handleItemInteraction} showObject={showObject} setShowObject={setShowObject} usedItems={usedItems} />
+          <Hotspot saveCameraState={saveCameraState} onClick={handleItemInteraction} onTouchEnd={handleTouchEnd} /> 
+        </Suspense>
+        {sphericalCoords && <ZoomControls targetSphericalCoords={sphericalCoords} zoomLevel={zoomLevel} zoomActive={zoomActive} />}
       </Canvas>
 
-      <ImageModal isVisible={isImageOpen} onClose={toggleImage} />
+      {/* <ImageModal isVisible={isImageOpen} onClose={toggleImage} /> */}
 
       {isDrawerOpen && (
         <TouchableWithoutFeedback onPress={toggleDrawer}>
           <View style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.7)', justifyContent: 'center' }}>
-            <SideDrawer isOpen={isDrawerOpen} items={interactedItems} selectedItem={CurrentSelectedItem} onLook={onLook} onUseItem={onUseItem} />
+            <SideDrawer isOpen={isDrawerOpen} items={interactedItems} selectedItem={CurrentSelectedItem} onLook={onLook} onUseItem={() => {onUseItem(CurrentSelectedItem); }}  />
           </View>
         </TouchableWithoutFeedback>
       )}
       <TouchableOpacity style={{ position: 'absolute', top: 30, left: 20 }} onPress={toggleDrawer}>
         <Image source={require('./assets/push_ui.png')} style={{ width: 40, height: 40 }} />
       </TouchableOpacity>
+
+      {isDialogueVisible && (
+        <Dialoguebox1
+          isVisible={isDialogueVisible}
+          content={dialogContent}
+          onClose={() => setIsDialogueVisible(false)}
+        />
+      )}
+      <TouchableOpacity onPress={handleBackButtonClick} style={{position: 'absolute', bottom: 50, right: 20}}>
+      <Image source={require('./assets/push_ui.png')} style={{ width: 40, height: 40 }} />
+      </TouchableOpacity>
     </View>
   );
 }
+
