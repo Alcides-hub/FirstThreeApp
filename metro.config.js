820f5a88e7fadac8c6db2a702f0f45e551501b84
alcides_t <alcides.dml@icloud.com>
2024-01-26 17:50:25 +0900

    Added components, dialogue, UI

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   resolver: {
     sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs'],
-    assetExts: ['glb', 'gltf', 'mtl', 'obj', 'png', 'jpeg', 'jpg', 'ttf'], // Added 'ttf' here
+    assetExts: ['glb', 'gltf', 'mtl', 'obj', 'png', 'jpeg', 'jpg', 'ttf', 'mp4'], // Add 'mp4' here
   },
 };
+
